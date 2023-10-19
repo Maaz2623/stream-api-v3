ad778ca24bba1125b8074f74b77efa098c149dc1
Maaz2623 <125387674+Maaz2623@users.noreply.github.com>
2023-10-19 21:28:59 +0530

    first commit

---
+++ src/app/api/movies/[slug]/route.js
@@ -0,0 +1,13 @@
+import { connectToDB } from "@/mongoose";
+import Movie from "@/models/movie";
+import { NextResponse } from "next/server";
+
+
+export async function GET(request, {params}){
+    const {slug} = params
+    await connectToDB()
+    const movie = await Movie.findOne({slug})
+    return NextResponse.json(movie, {status: 200})
+}
+
+
