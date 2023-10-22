532ee06098fa18f1a161d57709dbd272646ec661
Maaz2623 <125387674+Maaz2623@users.noreply.github.com>
2023-10-22 13:11:08 +0530

    modified:   src/app/api/create/route.js

--- src/app/api/create/route.js
+++ src/app/api/create/route.js
@@ -34,11 +34,6 @@ export async function POST(request) {
   });
   return NextResponse.json(newMovie, {
     status: 201,
-    headers: {
-      "Access-Control-Allow-Origin": "http://localhost:3000/create",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    },
   });
 }
 
